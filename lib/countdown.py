fa3f61c5ac66f916c94559c5ca51531d4e029d04
Emmi Lindsey <emmilindsey@gmail.com>
2023-12-11 13:30:41 -0700

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+def countdown(item):
+    while item > 0:
+        print (f"{item} SECOND(S)!")
+        item -= 1
+    print("HAPPY NEW YEAR!")
+
+    
+def countdown_with_sleep(item):
+    countdown(item)
+    import time
+    time.sleep(1)
+
+
+
+
+
